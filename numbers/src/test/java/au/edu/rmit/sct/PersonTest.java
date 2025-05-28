1d8a586c5c8a94d55f76e43b411fba8192f2043a
Rui-Geng <s3822749@student.rmit.edu.au>
2025-05-28 20:17:09 +1000

    Update PersonTest.java

--- numbers/src/test/java/au/edu/rmit/sct/PersonTest.java
+++ numbers/src/test/java/au/edu/rmit/sct/PersonTest.java
@@ -306,13 +306,13 @@ public class PersonTest {
 		@Test
         @Order(4)
 		void testAddPerson4() {
-			//the personID should be 10 characters long
-			Person tp4 = new Person("56s_d%&fA", "Grace", "Geng",
-				    "32|Highland Street|Melbourne|Queensland|Australia",  "15-11-1990", 
+			//add correct personal information into Person.txt
+			Person tp4 = new Person("56s_d%&fAB", "Grace", "Geng",
+				    "32|Highland Street|Melbourne|Victoria|Australia",  "15-11-1990", 
 				    "15-11-1990| 3|15-12-1990| 3",
 				    true);
-			System.out.println("testAddPerson4: the personID should be 10 characters long");
-			assertFalse(tp4.addPerson(filePath));
+			System.out.println("testAddPerson4: personal information added");
+			assertTrue(tp4.addPerson(filePath));
 			System.out.println("testAddPerson4: successful!");
 		}
 		
@@ -320,8 +320,8 @@ public class PersonTest {
         @Order(5)
 		void testAddPerson5() {
 			//the format of birthdate should follow DD-MM-YYYY
-			Person tp5 = new Person("56s_d%&fA", "Grace", "Geng",
-				    "32|Highland Street|Melbourne|Queensland|Australia",  "1990-11-15", 
+			Person tp5 = new Person("56s_d%&fAB", "Grace", "Geng",
+				    "32|Highland Street|Melbourne|Victoria|Australia",  "1990-11-15", 
 				    "15-11-1990| 3|15-12-1990| 3",
 				    true);
 			System.out.println("testAddPerson5: the format of birthdate should follow DD-MM-YYYY");
